18f234f1d1e5b58c03553d3b10256f2d27e91220
josueventura07 <josueandresventura@gmail.com>
2022-12-01 07:03:39 -0400

    Users_CRUD-spaghetti

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,13 @@
+const { Sequelize } = require('sequelize')
+
+const db = new Sequelize ({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'jb1786',
+    database: 'users-spaghetti'
+})
 
 
+module.exports = db
